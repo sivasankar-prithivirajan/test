669e5ce7dcbaa2e5f5b3edb002b3e3ff1c81778b
sivasankar-prithivirajan <sivasankarstorage@gmail.com>
2025-01-21 22:22:06 +0530

    blog

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2" color="red">
-            dfbvdfgng{" "}
+            new blog changes{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               src/app/page.tsx
             </code>
